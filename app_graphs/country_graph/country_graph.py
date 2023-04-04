4d9d2fd6135bc2c209f09c2fc13662aede9b5839
Arturo J. Alva <arturo.j.a.m@outlook.com>
2023-04-03 21:56:17 -0500

    agregando volumes en decoker-compose para darle 'hot-reload' al contenedor.

--- app_graphs/country_graph/country_graph.py
+++ app_graphs/country_graph/country_graph.py
@@ -1,11 +1,9 @@
+from utils.charts import generate_bar_chart
+from utils.read_csv import read_csv
+from utils.mod import get_population, get_data_by_country
 import sys
 # setting path
 sys.path.append('../')
-
-from utils.mod import get_population, get_data_by_country
-from utils.read_csv import read_csv
-from utils.charts import generate_bar_chart
-
 
 
 total_data = read_csv('../utils/data.csv')
@@ -15,4 +13,5 @@ country_data = get_data_by_country(total_data, country)
 # print(country_data)
 labels, values = get_population(country_data)
 # print(list(labels))
+# probando 22
 generate_bar_chart(list(labels), list(values), country)
